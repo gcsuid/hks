824b51e97d9fbda73ce48be1001a0fffd6f7badb
Ayush <dasayush483@gmail.com>
2026-03-10 22:45:41 +0530

    after week 2

---
+++ week3/middle.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const app = express()
+
+app.get("/", (req,res) => {
+    const usern = req.headers.username
+    const pw = req.headers.password
+    const kid = req.query.Kidneyid;
+
+    if (!(usern === 'ayush' && pw ==='nooo')){
+        res.status(400).json({msg:"youre dead dawg"})
+    return
+        }
+
+    if(kid != 1 || kid != 2){
+        res.status(400).json({msg:"youre dead dawg"})}
+
+    
+
+    res.json({msg:"youre dead dawg"
+
+    })
+
+
+})
